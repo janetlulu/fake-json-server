87c694604a11fb436b0bbecffa6da5a142c72c56
Janet Lu <program66519@gmail.com>
2022-08-23 10:02:04 +0800

    feat: add news and device to api

--- db.js
+++ db.js
@@ -1,7 +1,11 @@
 const posts = require("./data/posts");
 const users = require("./data/users");
+const news = require("./data/news");
+const device = require("./data/device");
 
 module.exports = {
     posts,
     users,
+    news,
+    device
 }
